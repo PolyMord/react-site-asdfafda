d89bc4aea174b51b0dd8a059ab0bf1175a98bb5c
PolyMord <stanislavmalickiy@gmail.com>
2020-03-18 19:51:31 +0300

    add scss

--- react-site/src/App.js
+++ react-site/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+function Header() {
+  return (
+    <header className="header">
+      <img src={logo} className="app-logo" alt="logo" />
+    </header>
+  )
+}
 
 export default function App() {
   return (
     <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-      </header>
+      <Header />
     </div>
   );
 }
