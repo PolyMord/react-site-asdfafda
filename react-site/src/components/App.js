256faef23de5897a158fc791b928ca279d70ac1f
PolyMord <stanislavmalickiy@gmail.com>
2020-03-28 03:07:12 +0300

    asd

--- react-site/src/components/App.js
+++ react-site/src/components/App.js
@@ -1,23 +1,69 @@
-import React, {useRef} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import './static/App.scss'
 import Header from './Header'
 import About from './About'
 import Skills from './Skills'
 import Services from './Services'
 import Menu from './Menu'
+import Loader from './Loader'
 
 export default function App() {
-  const scrollAbout = useRef(null)
-  const scrollSkills = useRef(null)
-  const scrollVasya = useRef(null)
+    const scrollAbout = useRef(null)
+    const scrollSkills = useRef(null)
+    const scrollVasya = useRef(null)
 
-  return (
-    <div className="app">
-      <Header />
-      <About refProp={scrollAbout} />
-      <Skills refProp={scrollSkills} />
-      <Services refProp={scrollVasya} />
-      <Menu refProp={[scrollAbout, scrollSkills, scrollVasya]} />
-    </div>
-  )
+    const [loader, setLoader] = useState(true)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setLoader(false)
+            clearTimeout(timeout)
+        }, 1500)
+    })
+
+    return (
+        loader ?
+        <Loader /> :
+        <div className="app">
+            <Header />
+            <About refProp={scrollAbout} />
+            <Skills refProp={scrollSkills} />
+            <Services refProp={scrollVasya} />
+            <Menu refProp={[scrollAbout, scrollSkills, scrollVasya]} />
+        </div>
+    )
 }
+
+
+
+// const [loader, setLoader] = useState(false)
+
+//     useEffect(() => {
+//         document.onreadystatechange = () => {
+//             if (document.readyState === "complete") {
+//                 setLoader(false)
+//             } else {
+//                 setLoader(true)
+//             }
+//         }
+//     })
+
+//     let loadApp;
+//     if (loader) {
+//         loadApp = (
+//             <div className="loader">
+//                 <div className="loader__spin"></div>
+//             </div>
+//         )
+
+//     } else {
+//         loadApp = (
+//             <>
+//                 <Header />
+//                 <About refProp={scrollAbout} />
+//                 <Skills refProp={scrollSkills} />
+//                 <Services refProp={scrollVasya} />
+//                 <Menu refProp={[scrollAbout, scrollSkills, scrollVasya]} />
+//             </>
+//         )
+//     }
--- react-site/src/components/Menu.js
+++ react-site/src/components/Menu.js
@@ -13,20 +13,54 @@ export default function Menu(props) {
     })
 
     const [about, skills, vasya] = props.refProp
-    const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop)
+    const scrollToRef = ref => window.scrollTo({ 
+        top: ref.current.offsetTop, 
+        behavior: 'smooth' 
+    })
     const scrollToAbout = () => scrollToRef(about) 
     const scrollToSkills = () => scrollToRef(skills) 
-    const scrollToVasya = () => scrollToRef(vasya) 
+    const scrollToVasya = () => scrollToRef(vasya)
     
     return (
         <div className="menu hidden">
             <ul className='menu__nav'>
-                <button onClick={scrollToAbout} >About</button>
-                <button onClick={scrollToSkills} >Skills</button>
-                <button onClick={scrollToVasya} >Vasya</button>
-                <button onClick={scrollToSkills} >Petya</button>
-                <button onClick={scrollToSkills} >Contacts</button>
+                <button
+                    className='btn'
+                    onClick={scrollToAbout} 
+                >
+                    About
+                </button>
+                <button
+                    className='btn' 
+                    onClick={scrollToSkills} 
+                >
+                    Skills
+                    </button>
+                <button
+                    className='btn' 
+                    onClick={scrollToVasya} 
+                >
+                    Services
+                </button>
+                <button
+                    className='btn' 
+                onClick={scrollToSkills} 
+                >
+                    Petya
+                </button>
+                <button
+                    className='btn' 
+                    onClick={scrollToSkills} 
+                >
+                    Contacts
+                </button>
             </ul>
+            <a 
+                className='menu__big-btn' 
+                href="https://play.typeracer.com"
+            >
+                TypeRacer
+            </a>
             <div className="menu__stub"></div>
         </div>
     )
